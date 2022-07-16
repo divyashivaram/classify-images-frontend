17de0320278e6feeeb2b90d3e0b0eaa985df56f3
Divya <div.shivaram@gmail.com>
2022-07-16 21:50:57 +0530

    add color

--- frontend/src/components/column.jsx
+++ frontend/src/components/column.jsx
@@ -7,8 +7,8 @@ const Container = styled.div`
   width: 500px;
   margin: auto;
   border-radius: 2px;
-  border: 1px solid ${blueGrey[100]};
-  background-color: ${blueGrey[300]};
+  border: 1px solid #af8eb5;
+  background-color:#e1bee7;
   
   /* we want the column to take up its full height */
   display: flex;
@@ -23,7 +23,7 @@ const Title = styled.h3`
 
 export default class Column extends Component {
   render() {
-    const columnName = this.props.column;
+    const columnName = this.props.column
     return (
       <Container>
         <Title>{columnName}</Title>
